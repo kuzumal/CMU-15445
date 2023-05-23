cf5e0b873720c5f78640ec859826a5ea0ed611f6
kuzumal <kuzuma@kuzumal>
2023-05-23 11:33:14 +0800

    p0_3

--- tst/1.cpp
+++ tst/1.cpp
@@ -4,9 +4,56 @@
 
 using namespace bustub;
 
+#define de(X) { std::cout << #X << " : " << X << std::endl; }
+
+#define ASSERT_EQ(a, b) assert((a) == (b))
+
+#define P(root, key, value) { root = std::make_shared<Trie>(std::move(root->Put(key, value))); }
+#define S(root, type, key) (root->Get<type>(key))
+#define R(root, key) { root = std::make_shared<Trie>(std::move(root->Remove(key))); }
+
 int main() {
     std::shared_ptr<Trie> root(new Trie);
-    std::map<int, int> m{std::pair<int, int>{1, 2}};
-    std::cout << m.count(2) << std::endl;
+    // std::ifstream test("./test.txt", std::ios::in);
+    // std::ofstream ans ("./my_result.txt", std::ios::out | std::ios::trunc);
+
+    // std::string mode, key, s; int val;
+    // while (std::getline(test, s)) { 
+    //     std::stringstream line(s);
+    //     line >> mode >> key >> val;
+    //     if (mode[0] == 'P') { P(root, key, val); }
+    //     else if (mode[0] == 'R') { R(root, key); }
+    //     else {
+    //         const int* temp = S(root, int, key);
+    //         assert(temp != nullptr);
+    //         ans << *temp << std::endl;
+    //     }
+    // }
+    // test.close(); ans.close();
+
+    P(root, "123", 123);
+    P(root, "12", 12);
+
+    R(root, "12");
+    
+    R(root, "123");
+    ASSERT_EQ(S(root, int, "12"), nullptr);
+    // auto trie = Trie();
+    // trie = trie.Put<uint32_t>("test", 2333);
+    // trie = trie.Put<uint32_t>("te", 23);
+    // trie = trie.Put<uint32_t>("tes", 233);
+
+
+    // trie = trie.Remove("te");
+    // ASSERT_EQ(trie.Get<uint32_t>("te"), nullptr);
+    // trie = trie.Remove("tes");
+    // ASSERT_EQ(*trie.Get<uint32_t>("te"), 23);
+    // ASSERT_EQ(trie.Get<uint32_t>("tes"), nullptr);
+    // trie = trie.Remove("test");
+    // ASSERT_EQ(trie.Get<uint32_t>("test"), nullptr);
+
+    // ASSERT_EQ(trie.Get<uint32_t>("te"), nullptr);
+    // ASSERT_EQ(trie.Get<uint32_t>("tes"), nullptr);
+    // ASSERT_EQ(trie.Get<uint32_t>("test"), nullptr);
     return 0; 
 }
